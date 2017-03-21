aa30a39bf96ef1865d128938ba20b570bbd1148f
rupurapu <tommivalkki@hotmail.com>
2017-03-21 14:10:45 +0200

    Juoksyhyppy lisätty, OnCollisionExit korjattu

--- PlayerJump.cs
+++ PlayerJump.cs
@@ -7,16 +7,14 @@ public class PlayerJump : MonoBehaviour {
     private bool isGrounded;
 
     //public GameObject ground;
-    public Vector3 jump;
     public Rigidbody rb;
-
-    public float jumpSpeed = 2.0f;
+    public float jumpSpeed;
+    public float jumpVerticalSpeed;
 
     // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        jump = new Vector3(0, 2.0f, 0);
     }
 
     // Update is called once per frame
@@ -24,8 +22,17 @@ public class PlayerJump : MonoBehaviour {
     {
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            //rb.AddForce(jump * jumpSpeed, ForceMode.Impulse);
+            float forwardDirection = Input.GetAxis("Vertical");
+            if (Input.GetKey("left shift"))
+            {
+                jumpVerticalSpeed = 10.0f;
+            }
+            else
+            {
+                jumpVerticalSpeed = 5.0f;
+            }
             rb.velocity += jumpSpeed * Vector3.up;
+            rb.velocity += forwardDirection * jumpVerticalSpeed * transform.forward;
             isGrounded = false;
         }
     }
--- PlayerMovement.cs
+++ PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour {
     private float speed;
     private bool isGrounded;
 
+    public Rigidbody rb;
     public float walkSpeed;
     public float runSpeed;
 
@@ -17,17 +18,20 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        speed = walkSpeed;
-
-        if (Input.GetKey("left shift") && isGrounded && Input.GetAxis("Vertical") >= 0)
+        if (isGrounded)
         {
-            speed = runSpeed;
-        }
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 100f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+            speed = walkSpeed;
+            if (Input.GetKey("left shift") && isGrounded && Input.GetAxis("Vertical") >= 0)
+            {
+                speed = runSpeed;
+            }
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 100f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        }
+        
     }
     void OnCollisionEnter(Collision col)
     {
@@ -40,7 +44,7 @@ public class PlayerMovement : MonoBehaviour {
     {
         if (col.gameObject.tag == "Ground")
         {
-            isGrounded = true;
+            isGrounded = false;
         }
     }
 }
