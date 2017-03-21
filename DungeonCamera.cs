c699d28e7e864ee014b2ee129557cb8baf519d0f
Tuomas R <Ravander@users.noreply.github.com>
2017-03-21 11:04:12 +0200

    pieni fixi kameraan

--- DungeonCamera.cs
+++ DungeonCamera.cs
@@ -14,15 +14,14 @@ public class DungeonCamera : MonoBehaviour {
     }
      
     void LateUpdate() {
-
         if (Input.GetAxis("Vertical") == 0) {
             float horizontal = Input.GetAxis("Horizontal") * rotateSpeed;
             transform.Rotate(0, horizontal, 0);
         } else {
             float currentAngle = transform.eulerAngles.y;
-float desiredAngle = target.transform.eulerAngles.y;
-float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
-Quaternion rotation = Quaternion.Euler(0, angle, 0);
+			float desiredAngle = target.transform.eulerAngles.y;
+			float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+			Quaternion rotation = Quaternion.Euler(0, angle, 0);
             transform.position = target.transform.position - (rotation * offset);
             transform.LookAt(target.transform, Vector3.up);
         }
