9d67f33a8ab66fabff302efe165c7e8dd8197731
rupurapu <tommivalkki@hotmail.com>
2017-03-21 11:01:01 +0200

    Hyppy ja liikkuminen v1

---
+++ PlayerJump.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour {
+
+    private bool isGrounded;
+
+    //public GameObject ground;
+    public Vector3 jump;
+    public Rigidbody rb;
+
+    public float jumpSpeed = 2.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0, 2.0f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(jump * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private float speed;
+    private bool isGrounded;
+
+    public float walkSpeed;
+    public float runSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        speed = walkSpeed;
+
+        if (Input.GetKey("left shift") && isGrounded && Input.GetAxis("Vertical") >= 0)
+        {
+            speed = runSpeed;
+        }
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 100f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+    void OnCollisionEnter()
+    {
+        isGrounded = true;
+    }
+    void OnCollisionExit()
+    {
+        isGrounded = false;
+    }
+}
