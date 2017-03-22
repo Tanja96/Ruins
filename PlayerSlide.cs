2d75af2ba1f6d2bee0cd249af5b5ee52c2c2580f
Tuomas R <Ravander@users.noreply.github.com>
2017-03-22 14:14:05 +0200

    EITOIMIVA slide v1 (left ctrl)

---
+++ PlayerSlide.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSlide : MonoBehaviour {
+
+	public float slideSpeed = 6.0f;
+	public float forwardJumpSpeed = 6.0f;
+	public float jumpHeight = 2.0f;
+	
+	public Rigidbody rb;
+
+	private bool sliding = false;
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (sliding) {
+			//transform.Translate(0, 0, slideSpeed);
+		}
+		
+		if (Input.GetKeyDown("left ctrl")) {
+			if (!sliding) {
+				rb.velocity += jumpHeight * Vector3.up;
+				rb.velocity += forwardJumpSpeed * transform.forward;
+				transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y / 3, transform.localScale.z);
+				
+				sliding = true;
+			}
+		}
+		if (Input.GetKeyUp("left ctrl")) {
+			if (sliding) {
+				transform.Translate(0, 2, 0);
+				transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * 3, transform.localScale.z);
+				sliding = false;
+			}
+		}
+	}
+}
