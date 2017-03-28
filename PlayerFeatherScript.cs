0598daea89316fa2f8f3a0f38d48f4512a6fb7dc
Tuomas R <Ravander@users.noreply.github.com>
2017-03-28 14:30:45 +0300

    Yhdistelin featherscriptin yhdeksi kompaktiksi skriptiksi.
    Helpompi yhdistellä muiden skriptien kanssa. Jos pelajaa "kerää" Feather-tagisen sulan sulkacount kasvaa.

---
+++ PlayerFeatherScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeatherScript : MonoBehaviour {
+
+	public int featherCount;
+
+	// Use this for initialization
+	void Start () {
+		
+		featherCount = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Feather") {
+			featherCount++;
+			Destroy(col.gameObject);
+		}
+	}
+	
+}
