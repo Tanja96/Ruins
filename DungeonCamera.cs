c66e67757cc267aef573d57845978cd1141674db
Tuomas R <Ravander@users.noreply.github.com>
2017-03-29 13:44:17 +0300

    Seuraa pelaajaa, free look kun rightklikkaa

--- DungeonCamera.cs
+++ DungeonCamera.cs
@@ -1,30 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class DungeonCamera : MonoBehaviour {
 
-    public GameObject target;
-    public float rotateSpeed = 5.0f;
-    public float damping = 0.9f;
-    Vector3 offset;
-     
+    public GameObject player;
+	public float rotateSpeed = 5f;
+	
+    private Vector3 offset;
+ 
     void Start() {
-        offset = target.transform.position - transform.position;
+        offset = player.transform.position - transform.position;
     }
-     
-    void LateUpdate() {
-        if (Input.GetAxis("Vertical") == 0) {
-            float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+	
+	void LateUpdate() {
+		
+		float desiredAngle = player.transform.eulerAngles.y;
+		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+		transform.position = player.transform.position - (rotation * offset);
+			
+		if (Input.GetKey("mouse 1")) {
+			float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
 			float vertical = -Input.GetAxis("Mouse Y") * rotateSpeed;
-            transform.Rotate(vertical, horizontal, 0);
-        } else {
-            float currentAngle = transform.eulerAngles.y;
-			float desiredAngle = target.transform.eulerAngles.y;
-			float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
-			Quaternion rotation = Quaternion.Euler(0, angle, 0);
-            transform.position = target.transform.position - (rotation * offset);
-            transform.LookAt(target.transform, Vector3.up);
-        }
-    }
+			
+			transform.RotateAround(player.transform.position, Vector3.up, horizontal);
+			transform.RotateAround(player.transform.position, transform.right, vertical);
+		} else {
+			transform.LookAt(player.transform);
+		}
+	}
 }
