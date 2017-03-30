fbb03a85ff0f1796655fa43de481173586b87fbd
rupurapu <tommivalkki@hotmail.com>
2017-03-30 14:39:28 +0300

    Parempi main menu rakenne v1 + navigointi menussa nuolinäppäimin

---
+++ MainMenuV2Script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuV2Script : MonoBehaviour
+{
+
+    public void startNewGame(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void exitToDesktop()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+};
