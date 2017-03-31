057ec65f3d5b053a7a13e88104a08b69056debfb
Tuomas R <Ravander@users.noreply.github.com>
2017-03-31 22:52:45 +0300

    PlayerRespawn v1
    Kun pelaaja "irtoaa" maasta (Ground-tag), positio tallentuu johon palataan kun törmätään Death-tag triggeriin.

---
+++ PlayerRespawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRespawn : MonoBehaviour {
+
+    private Vector3 lastPosition;
+    private Rigidbody rb;
+
+	void Start () {
+        lastPosition = transform.position;
+        rb = GetComponent<Rigidbody>();	
+	}
+
+    void OnCollisionExit(Collision col) {
+        if (col.gameObject.tag == "Ground") {
+            lastPosition = transform.position;
+        }
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.gameObject.tag == "Death") {
+            rb.velocity = new Vector3(0, 0, 0);
+            transform.position = lastPosition;
+        }
+    }
+}
