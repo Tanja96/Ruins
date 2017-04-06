de2ed476be0f026f8bdd05d959b9e38289feadba
Tuomas R <Ravander@users.noreply.github.com>
2017-04-06 12:35:47 +0300

    Freelook kamera siirtyy pelaajan "silmiin"

--- DungeonCamera.cs
+++ DungeonCamera.cs
@@ -4,21 +4,28 @@ using System.Collections;
 public class DungeonCamera : MonoBehaviour {
 
     private Vector3 offset;
+    private Vector3 def_offset;
+    private Vector3 zoom_offset;
 
     public GameObject player;
 	public float rotateSpeed = 5f;
 
     void Start() {
         offset = player.transform.position - transform.position;
+        def_offset = offset;
+        zoom_offset = offset - new Vector3(0, offset.y + 0.6f, offset.z);
     }
 	
 	void LateUpdate() {
-		
-		float desiredAngle = player.transform.eulerAngles.y;
+        
+        float desiredAngle = player.transform.eulerAngles.y;
 		Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-		transform.position = player.transform.position - (rotation * offset);
-			
+        transform.position = player.transform.position - (rotation * offset);
+        
 		if (Input.GetKey("mouse 1")) {
+            
+            offset = zoom_offset; 
+            
 			float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
 			float vertical = -Input.GetAxis("Mouse Y") * rotateSpeed;
 			
@@ -26,6 +33,8 @@ public class DungeonCamera : MonoBehaviour {
 			transform.RotateAround(player.transform.position, transform.right, vertical);
 		} else {
 			transform.LookAt(player.transform);
+            offset = def_offset;
 		}
+        
 	}
 }
