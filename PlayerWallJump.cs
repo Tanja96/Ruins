cde339f496e094eccb5f991475418883073328f9
rupurapu <tommivalkki@hotmail.com>
2017-04-18 11:07:04 +0300

    Seinähyppy v1

---
+++ PlayerWallJump.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWallJump : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    public float wallJumpSpeed = 10.0f;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        float t = Time.time;
+        if(col.gameObject.CompareTag("Ground") && Input.GetButton("Jump")){
+            rb.velocity = new Vector3(0, 0, 0);
+            if(Time.time < t + 3)
+            {
+                rb.velocity += wallJumpSpeed * Vector3.up;
+                rb.velocity += wallJumpSpeed / 2 * vector3.back;
+            }
+            else
+            {
+                rb.velocity += wallJumpSpeed / 5 * Vector3.down;
+            }
+        }
+    }
+}
