791675a3e07282a2b0aeb1a9a523c374c5aeb650
rupurapu <tommivalkki@hotmail.com>
2017-05-04 10:08:06 +0300

    v1

---
+++ PlayerSwinging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSwinging : MonoBehaviour {
+
+    private CharacterController controller;
+
+    void Start() {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+
+    void OnTriggerEnter(Collider col) {
+        if (Input.GetMouseButtonDown(1) && col.CompareTag("Liana")) {
+            transform.SetParent(col.gameObject.transform);
+        }
+    }
+    void OnTriggerStay(Collider col) {
+        if (Input.GetMouseButtonDown(1) && col.CompareTag("Liana")) {
+            transform.SetParent(col.gameObject.transform);
+        }
+    }
+    void OnTriggerExit(Collider col) {
+        if (!Input.GetMouseButtonDown(1) && col.CompareTag("Liana")) {
+            transform.SetParent(null);
+        }
+    }
+}
